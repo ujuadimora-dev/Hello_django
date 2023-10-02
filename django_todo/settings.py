00b3ed1fa0841fcb706343f1a95b3f4d4b303b0c
Adimora Uju Louisa <adimorauju@gmail.com>
2023-10-02 00:40:44 +0000

    set up the envromrnt variable

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -17,6 +17,7 @@ import dj_database_url
 if os.path.isfile("env.py"):
     import env
 
+development = os.environ.get('DEVELOPMEMT', False)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,9 +31,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
-ALLOWED_HOSTS = [ 'Localhost','what-todo-app-02e03e363fc8.herokuapp.com','8000-ujuadimorad-hellodjango-h4ynaloledu.ws-eu105.gitpod.io']
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = ['what-todo-app-02e03e363fc8.herokuapp.com','8000-ujuadimorad-hellodjango-h4ynaloledu.ws-eu105.gitpod.io']
 
 
 # Application definition
@@ -80,17 +84,17 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-  'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 
